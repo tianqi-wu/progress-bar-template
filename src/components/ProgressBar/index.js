6d90efc5a74200bde7267899ee4961ad5b335e06
Scott Massey <scott.massey@wustl.edu>
2021-06-01 16:03:04 -0500

    Added prettier, refactored component to be a functional component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
-class ProgressBar extends Component {
-	render() {
-		const { color, text } = this.props
+const ProgressBar = props => {
+  const { color, text } = props
 
-		return (
-			<View style={styles.wrapper}>
-				<Text style={{ color }}>{text}</Text>
-			</View>
-		)
-	}
+  return (
+    <View style={styles.wrapper}>
+      <Text style={{ color }}>{text}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	wrapper: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 })
 
 export default ProgressBar

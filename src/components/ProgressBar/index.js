08cb67648ab9260106eb8994cb0b48f0c32f1442
Tianqi Wu <tianqiwu@TianqideAir2121.attlocal.net>
2021-06-07 18:06:46 -0500

    Day 1

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
-  const { color, text } = props
+  const { color_unfinished, color_finished, text, percentage, height, isNumberShown } = props
+  const [percentage_state, setPercentage] = useState(percentage);
+  const radius = height / 2;
 
-  return (
-    <View style={styles.wrapper}>
-      <Text style={{ color }}>{text}</Text>
-    </View>
-  )
+  
+  if(percentage > 100 || percentage < 0) {
+    return (
+        <Text style={{color: 'red'}}>Invalid Result</Text>
+    )} else {
+      return (
+    
+
+    
+      <>
+        <View style={{ flexDirection: styles.row_wrapper.flexDirection, height: height, overflow: 'hidden'}}>
+        <View style={{ backgroundColor: color_finished, flex: percentage / 100, borderTopLeftRadius: radius, borderBottomLeftRadius: radius, borderWidth: 2, borderColor: 'gold'}}></View>
+        <View style={{ backgroundColor: color_unfinished, flex: (100 - percentage) / 100, borderTopRightRadius: radius,  borderBottomRightRadius: radius, borderWidth: 2, borderColor: 'gold' }}></View>
+        {isNumberShown && <Text style={styles.wrapper}>{ `${percentage}%`}</Text>}
+        </View>
+      
+      </>
+      )
+    }
+  
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +33,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row_wrapper: {
+    flexDirection: 'row',
   },
 })
 

df428d03c31a524d6e77fbae25dac21bb2d93fde
Tianqi Wu <tianqiwu@TianqideAir2121.attlocal.net>
2021-06-08 13:20:14 -0500

    Day 2

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,31 +1,71 @@
-import React, {useState} from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { Animated, Text, View, StyleSheet } from 'react-native'
 
 const ProgressBar = props => {
-  const { color_unfinished, color_finished, text, percentage, height, isNumberShown } = props
-  const [percentage_state, setPercentage] = useState(percentage);
-  const radius = height / 2;
+  const { color_unfinished, color_finished, text, percentage, isNumberShown } =
+    props
+  const [percentage_state, setPercentage] = useState(percentage)
+  const [width, setWidth] = useState(240)
+  const height = 24
+  const radius = height / 2
 
-  
-  if(percentage > 100 || percentage < 0) {
-    return (
-        <Text style={{color: 'red'}}>Invalid Result</Text>
-    )} else {
-      return (
-    
+  animVal = new Animated.Value(0);
 
-    
-      <>
-        <View style={{ flexDirection: styles.row_wrapper.flexDirection, height: height, overflow: 'hidden'}}>
-        <View style={{ backgroundColor: color_finished, flex: percentage / 100, borderTopLeftRadius: radius, borderBottomLeftRadius: radius, borderWidth: 2, borderColor: 'gold'}}></View>
-        <View style={{ backgroundColor: color_unfinished, flex: (100 - percentage) / 100, borderTopRightRadius: radius,  borderBottomRightRadius: radius, borderWidth: 2, borderColor: 'gold' }}></View>
-        {isNumberShown && <Text style={styles.wrapper}>{ `${percentage}%`}</Text>}
-        </View>
-      
-      </>
-      )
+
+  const handleLayout = ({ nativeEvent }) => {
+    const { width } = (nativeEvent && nativeEvent.layout) || {}
+    const { prevWidth } = width
+
+    if (width !== prevWidth) {
+      setWidth(width)
     }
-  
+  }
+
+  if (width == null) {
+    return <View></View>
+  }
+
+  if (percentage > 100 || percentage < 0) {
+    return <Text style={{ color: 'red' }}>Invalid Result</Text>
+  } else {
+    return (
+      <>
+        <View
+          style={{
+            flexDirection: styles.row_wrapper.flexDirection,
+            height: height,
+            overflow: 'hidden',
+          }}
+          onLayout={handleLayout}
+          //style={{width: width}}
+        >
+          <View
+            style={{
+              backgroundColor: color_finished,
+              flex: percentage / 100,
+              borderTopLeftRadius: radius,
+              borderBottomLeftRadius: radius,
+              borderWidth: 2,
+              borderColor: 'gold',
+            }}
+          ></View>
+          <View
+            style={{
+              backgroundColor: color_unfinished,
+              flex: (100 - percentage) / 100,
+              borderTopRightRadius: radius,
+              borderBottomRightRadius: radius,
+              borderWidth: 2,
+              borderColor: 'gold',
+            }}
+          ></View>
+          {isNumberShown && (
+            <Text style={styles.wrapper}>{`${percentage}%`}</Text>
+          )}
+        </View>
+      </>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
